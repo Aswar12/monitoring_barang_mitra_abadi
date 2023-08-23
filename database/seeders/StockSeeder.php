289a6fddc52543dd70cc39dff8c1633e0283971e
fialsyahran1 <142391252+fialsyahran1@users.noreply.github.com>
2023-08-23 21:02:38 +0800

    seeder

---
+++ database/seeders/StockSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class StockSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Stock::factory()->count(10)->create(); // Membuat 10 data stok barang dengan menggunakan factory
+    }
+}
