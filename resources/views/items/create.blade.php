2cc0f55a82db6562753f4a648115ceffd3721470
FIRMANGANTENG46 <firmansyah18@mhs.akba.ac.id>
2023-09-05 10:26:50 +0800

    d

--- resources/views/items/create.blade.php
+++ resources/views/items/create.blade.php
@@ -1,32 +1,32 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Tambah Item</title>
-</head>
-<body>
-    <h1>Tambah Item</h1>
-    @if ($errors->any())
-        <div style="color: red;">
-            <ul>
-                @foreach ($errors->all() as $error)
+@extends('../layouts.app')
+@section('content')
+<div class="py-12 ">
+    <div class="mx-auto max-w-7xl sm:px-6 lg:px-4 dark:bg-gray-500">
+        <div class="overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-500 sm:rounded-lg ">
+            <h1>Tambah Item</h1>
+            @if ($errors->any())
+            <div>
+                <ul>
+                    @foreach ($errors->all() as $error)
                     <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+                    @endforeach
+                </ul> </div>
+            @endif
+            <form action="/items" method="POST">
+                @csrf
+                <label for="name">Nama:</label>
+                <input type="text" name="name" id="name" required>
+                <br>
+                <label for="description">Deskripsi:</label>
+                <input type="text" name="description" id="description" required>
+                <br>
+                <label for="unit_price">Harga:</label>
+                <input type="number" name="unit_price" id="unit_price" required>
+                <br>
+                <button type="submit">Simpan</button>
+            </form>
+            <a href="/items">Kembali ke Daftar Item</a>
         </div>
-    @endif
-    <form action="/items" method="POST">
-        @csrf
-        <label for="name">Nama:</label>
-        <input type="text" name="name" id="name" required>
-        <br>
-        <label for="deskription">Deskripsi:</label>
-        <input type="text" deskription="deskription" id="deskription" required>
-        <br>
-        <label for="price">Harga:</label>
-        <input type="number" name="price" id="price" required>
-        <br>
-        <button type="submit">Simpan</button>
-    </form>
-    <a href="/items">Kembali ke Daftar Item</a>
-</body>
-</html>
+    </div>
+</div>
+@endsection
--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,28 +1,31 @@
 @extends('layouts.app')
 @section('content')
 <div class="py-12 ">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-4 dark:bg-gray-500">
-        <div class="bg-white dark:bg-gray-500 overflow-hidden shadow-xl sm:rounded-lg rounded-lg ">
-            <h1 class="text-center text-lg font-bold mb-2 py-2">Daftar Barang</h1>
-
+    <div class="mx-auto max-w-7xl sm:px-6 lg:px-4 dark:bg-gray-500">
+        <div class="overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-500 sm:rounded-lg ">
+            <h1 class="py-2 mb-2 text-lg font-bold text-center">Daftar Barang</h1>
+            {{-- //buatkan tombol tambah data --}}
+            <div class="flex justify-end px-4 py-3 text-right sm:px-6">
+                <a href="/items/create" class="px-4 py-2 font-bold text-white bg-black rounded">Tambah Data</a>
+            </div>
             <div class="overflow-x-auto dark:bg-gray-600">
-                <table class="min-w-full border dark:border-gray-300 rounded-lg ">
+                <table class="min-w-full border rounded-lg dark:border-gray-300 ">
                     <thead>
-                        <tr class="bg-gray-100 dark:bg-gray-100 rounded-lg ">
-                            <th class="border px-6 py-3 dark:border-black">#</th>
-                            <th class="border px-6 py-3 dark:border-black">Nama</th>
-                            <th class="border px-6 py-3 dark:border-black">Deskripsi</th>
-                            <th class="border px-6 py-3 dark:border-black">Harga Satuan</th>
-                            <th class="border px-6 py-3 dark:border-black">Tindakan</th>
+                        <tr class="bg-gray-100 rounded-lg dark:bg-gray-100 ">
+                            <th class="px-6 py-3 border dark:border-black">#</th>
+                            <th class="px-6 py-3 border dark:border-black">Nama</th>
+                            <th class="px-6 py-3 border dark:border-black">Deskripsi</th>
+                            <th class="px-6 py-3 border dark:border-black">Harga Satuan</th>
+                            <th class="px-6 py-3 border dark:border-black">Tindakan</th>
                         </tr>
                     </thead>
                     <tbody>
                         @foreach ($items as $index => $item)
                         <tr class="bg-white dark:bg-gray-600">
-                            <td class="border px-6 py-3 dark:border-white">{{ $index + 1 }}</td>
-                            <td class="border px-6 py-3 dark:border-white">{{ $item->name }}</td>
-                            <td class="border px-6 py-3 dark:border-white">{{ $item->description }}</td>
-                            <td class="border px-6 py-3 dark:border-white">{{ $item->unit_price }}</td>
+                            <td class="px-6 py-3 border dark:border-white">{{ $index + 1 }}</td>
+                            <td class="px-6 py-3 border dark:border-white">{{ $item->name }}</td>
+                            <td class="px-6 py-3 border dark:border-white">{{ $item->description }}</td>
+                            <td class="px-6 py-3 border dark:border-white">{{ $item->unit_price }}</td>
                             <td>
                     <a href="/items/{{ $item->id }}">Detail</a>
                     <a href="/items/{{ $item->id }}/edit">Edit</a>
