67f2eb1404fe2f35efb3ccbe45c9a297dee3bc6d
FIRMANGANTENG46 <firmansyah18@mhs.akba.ac.id>
2023-09-07 21:13:16 +0800

    jgn dignggu

--- resources/views/items/edit.blade.php
+++ resources/views/items/edit.blade.php
@@ -1,8 +1,12 @@
 @extends('layouts.app')
 @section('content')
-    <h1>Edit Item</h1>
-    <form action="/items/{{ $item->id }}" method="POST">
-        @csrf
+    <div class="py-12 ">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-4 dark:bg-gray-500">
+            <div class="overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-500 sm:rounded-lg ">
+
+                <h5>Edit Item</h5>
+                {{-- <form action="{{ route('items.update', $item->id) }}" method="POST"> --}}
+    @csrf
         @method('PUT')
         <div class="grid grid-cols-2 gap-2">
             <div class="p-2">
@@ -21,8 +25,8 @@
                     <label for="quantity">Item Quantity</label>
                     <input type="text" class="w-full p-2 border rounded shadow" name="quantity" value="{{ $item->quantity }}">
                 </div>
-                <div>
-                <button type="submit" class="text-white bg-black ">Update Item</button>
+                <div class="flex justify-end px-4 py-3 text-right sm:px-6">
+                    <a href="#" class="px-4 py-2 font-bold text-white bg-black rounded">Update data</a>
                 </div>
                 <p></p>
             @endsection
